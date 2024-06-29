adaa7e0d5601d2f582d31cdbe45933195df18830
Emir <ebasic@gmail.com>
2024-06-29 20:53:49 +0200

    feat: add shuffle ability

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script>
+import { _ } from 'lodash'
 import { computed, ref, watch } from 'vue'
 import Card from './components/Card.vue'
 export default {
@@ -24,10 +25,14 @@ export default {
       return remainingCards / 2
     })
 
+    const shuffleCards = () => {
+      cardList.value = _.shuffle(cardList.value)
+    }
+
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
-        value: 3,
-        visible: false,
+        value: i,
+        visible: true,
         position: i,
         matched: false
       })
@@ -71,7 +76,8 @@ export default {
       cardList,
       flipCard,
       userSelection,
-      status
+      status,
+      shuffleCards
     }
   }
 }
@@ -94,6 +100,7 @@ export default {
   </section>
 
   <h3>{{ status }}</h3>
+  <button @click="shuffleCards">Shuffle Cards</button>
 </template>
 
 <style>
