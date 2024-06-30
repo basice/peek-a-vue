77aacb76a581bdba8d3255fb78d9a54f7707eadb
Emir <ebasic@gmail.com>
2024-06-30 17:14:21 +0200

    refactor: create card component with value prop

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+<template>
+  <div class="card">
+    {{ value }}
+  </div>
+</template>
+
+<style></style>
